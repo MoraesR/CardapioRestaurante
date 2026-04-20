3ea7dc3075727deb22ac544d71240c0a505cd4bf
Rodrigo Moraes <rodrigomoraes174@gmail.com>
2026-04-20 13:29:32 -0400

    App cardapio de restaurante

--- RestauranteApp/restaurante.py
+++ RestauranteApp/restaurante.py
@@ -1,7 +1,17 @@
+from RestauranteApp.avaliacao import avaliacao
 
 
 class Restaurante:
     pass
-    restaurante =[]
-    
+    restaurantes =[]
+
+    def __init__(self, nome, categoria):
+        self._nome = nome
+        self._categoria = categoria
+        self._ativo = False
+        self.avaliacao = []
+        Restaurante.restaurantes.append(self)
+        
+    def __str__(self):
+        return f'{self._nome} | {self._categoria}'
 
